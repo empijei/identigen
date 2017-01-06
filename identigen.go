b1d1efcb9d3ffd7a14b588da6b57b6eacafcda95
AnnaOpss <hi@anna.so>
2017-01-06 21:29:33 +0100

    Added partial marsheling

--- identigen.go
+++ identigen.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/empijei/identigen/identities"
 )
@@ -14,6 +16,15 @@ var maxage = flag.Int("maxage", 50, "The maximum age for random people generatio
 var n = flag.Int("n", 1, "The amount of random people to generate. Must be positive.")
 
 func main() {
+	type toPrint struct {
+		CodiceFiscale    string
+		PartitaIva       string
+		ComunePartitaIva string
+		Documento        string
+		CartaCredito     string
+	}
+	var items toPrint
+
 	flag.Parse()
 	if *minage >= *maxage || *n <= 0 {
 		flag.PrintDefaults()
@@ -24,6 +35,20 @@ func main() {
 		pi, county, _ := ppl.PartitaIva()
 		id := ppl.ID()
 		cc, _ := ppl.CartaCredito()
-		fmt.Printf("%v, CF: %v, ID: %v, Partita iva: %v, emessa in %s, Carta di Credito: %v", ppl, cf, id, pi, county, cc)
+		fmt.Printf("%v\n", ppl)
+
+		items = toPrint{
+			CodiceFiscale:    cf,
+			PartitaIva:       pi,
+			ComunePartitaIva: county,
+			Documento:        id,
+			CartaCredito:     cc,
+		}
 	}
+
+	b, err := json.MarshalIndent(items, " ", "   ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
 }
