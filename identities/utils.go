6b8c648299bc25bb8578e4b1adbca1e4ae91aea9
RobClap <robclap8@gmail.com>
2017-01-10 18:14:52 +0100

    Added phone number generation

--- identities/utils.go
+++ identities/utils.go
@@ -1,7 +1,18 @@
 package identigen
 
-import "math"
+import (
+	"math"
+	"math/rand"
+)
 
 func nthdigit(num, pos int) int {
 	return int(float64(num)/math.Pow10(pos)) % 10
+}
+
+func randString(charSet []rune, length int) string {
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = charSet[rand.Intn(len(charSet))]
+	}
+	return string(b)
 }
