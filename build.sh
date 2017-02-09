fda5fb9152b2e81050e7b9764622296ff62edd4e
RobClap <robclap8@gmail.com>
2017-02-09 17:15:07 +0100

    This allows for local builds

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-for os in darwin freebsd windows
+for os in darwin freebsd
 do 
 	for arch in amd64 386
 	do 
 		mkdir -p "bin/$os/$arch/"
 		GOOS=$os GOARCH=$arch go build -o "bin/$os/$arch/identigen"
 	done
+done
+
+for arch in amd64 386
+do 
+	mkdir -p "bin/windows/$arch/"
+	GOOS=windows GOARCH=$arch go build -o "bin/windows/$arch/identigen.exe"
 done
