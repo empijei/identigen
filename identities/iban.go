8825302998f01992a327204bbc27bbe1ba1d0097
AnnaOpss <hi@anna.so>
2017-02-22 16:13:45 +0100

    Working IBAN

--- identities/iban.go
+++ identities/iban.go
@@ -1,63 +1,100 @@
 package identities
 
 import (
-	"errors"
-	"fmt"
 	"math/big"
 	"strconv"
-
-	"github.com/empijei/identigen/identities/lists"
+	"unicode"
 )
 
+const country = "IT" // iso for italy
+
 func (p *Person) IBAN() (iban string, err error) {
-	//if p.iban != "" {
-	//	return p.iban, nil
-	//}
+	if p.iban != "" {
+		return p.iban, nil
+	}
 
-	//bbac := fmt.Sprintf(rand.Int()%100000, rand.Int()%100000, rand.Int()%1000000000000)
+	abi := "05428"
+	cab := "11101"
+	cc := "000000123456"
 	// bbac := "0542811101000000123456" // cci should be X and CCI 60
-	// bbac := "1234512345123456789012" // cci should be L and CCI 02
 	// bbac := "0326848670052319093140"   // cci should be A and CCI 07
-	bbac := "0301503200000003564232"   // cci should be W and CCI 24
-	cci := checkDigit(BBAC(bbac)) + 10 //Convert letter to double digit number
-	country := 1930                    //IT converted with the (nth + 10) letter of the alphabet (I=19, T=30)
-
-	//	toCheck := strconv.Itoa(cci) + bbac + strconv.Itoa(country) + strconv.Itoa(checks) + strconv.Itoa(checks)
-
-	toCheck, ok := (&big.Int{}).SetString(strconv.Itoa(cci)+bbac+strconv.Itoa(country)+"00", 10)
-
-	if !ok {
-		return "", errors.New("Conversion to bigInt failed")
-	}
-	//log.Println(toCheck)
-	//toCheck.SetString("330542811101000000123456193000", 10)
-	checks := 98 - int((&big.Int{}).Mod(toCheck, big.NewInt(97)).Uint64())
-
-	sChecks := fmt.Sprintf("%02d", checks) //Padding
-	//log.Println(sChecks)
-	return "IT" + sChecks + string(int('A')+cci-10) + bbac, nil
+	//bbac := "0301503200000003564232"   // cci should be W and CCI 24
+	iban = GenerateIban(abi, cab, cc)
+	p.iban = iban
+	return
 }
 
-type BBAC string
+func GenerateIban(abi, cab, cc string) string { // Expecting ABI, CAB and cc to be padded with zeros
+	cin := cin(abi, cab, cc)
+	bban := cin + abi + cab + cc
+	cd := checkDigit(bban)
+	iban := country + cd + cin + abi + cab + cc
 
-func (b *BBAC) Digit(n int) int {
-	d, _ := strconv.Atoi(string((*b)[n]))
-	return d
+	return iban
+
 }
 
-func (b *BBAC) ConvertedDigit(n int) int {
-	if x := n % 2; x == 0 {
-		return lists.CCI[b.Digit(n)]
-	} else {
-		return b.Digit(n)
+func checkDigit(bban string) string {
+	iban_tmp := bban + "IT00"
+	var ret string
+	aux := ""
+
+	for _, elem := range iban_tmp {
+		if unicode.IsLetter(elem) {
+			var tmp string
+			tmp = strconv.Itoa(int(elem - 65 + 10))
+			if len(tmp) == 1 {
+				tmp = "0" + tmp
+			}
+			aux += tmp
+
+		} else {
+			aux += string(elem)
+		}
 	}
+
+	// Idk what i'm doing
+	bf_number := new(big.Int)
+	mod := new(big.Int)
+	result := new(big.Int)
+	div := big.NewInt(97)
+	sub := big.NewInt(98)
+	// SO MANY BIGINT
+
+	bf_number.SetString(aux, 10)
+
+	mod.Mod(bf_number, div)
+	result.Sub(sub, mod)
+	ret = result.String()
+
+	// Padding with 0
+	if len(ret) == 1 {
+		ret = "0" + ret
+	}
+	return ret
 }
 
-func checkDigit(bbac BBAC) int {
-	result := 0
-	for x := range bbac {
-		result += bbac.ConvertedDigit(x)
+func cin(abi, cab, cc string) string {
+	w_even := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28}
+	w_odd := []int{1, 0, 5, 7, 9, 13, 15, 17, 19, 21, 2, 4, 18, 20, 11, 3, 6, 8, 12, 14, 16, 10, 22, 25, 24, 23, 27, 28, 26}
+
+	var p int
+	iban_tmp := abi + cab + cc
+	total_weight := 0
+
+	for i, elem := range iban_tmp {
+
+		if unicode.IsLetter(elem) {
+			p = int(elem - 65)
+		} else {
+			p = int(elem - 48)
+		}
+
+		if (i % 2) == 0 { // if i is even i+1 is odd
+			total_weight += w_odd[p]
+		} else {
+			total_weight += w_even[p]
+		}
 	}
-	result = result % 26
-	return result
+	return string((total_weight % 26) + 65)
 }
