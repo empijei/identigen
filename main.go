dc0f4992da1fea5ed4c4edcbe263f01ce6b91134
empijei <empijei@gmail.com>
2017-03-01 23:00:47 +0100

    Added help for XML

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var minage = flag.Int("minage", 25, "The minimum age for random people generatio
 var maxage = flag.Int("maxage", 55, "The maximum age for random people generation. Must be positive, less or equal than 200 and more than minage.")
 var number = flag.Int("number", 1, "The amount of random people to generate. Must be positive.")
 var dt_fmt = flag.String("dt_fmt", "eu", "The format of the dates. Supports: 'eu','us','ja'")
-var format = flag.String("format", "human", "The comma separated list of formats for the output. Supports: 'json', 'csv', 'human'.")
+var format = flag.String("format", "human", "The comma separated list of formats for the output. Supports: 'json', 'csv', 'xml', 'human'.")
 var fields = flag.String("fields", "all", "The comma separated case-sensitive list of fields to print. Use 'all' to print all of them. Supported fields are: "+strings.Join(identities.AllFields, ","))
 
 func main() {
