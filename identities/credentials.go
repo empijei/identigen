99487abb73d679c64b9735eedce47c4c7aa1e413
AnnaOpss <hi@anna.so>
2017-03-02 16:51:13 +0100

    Check for usernames

--- identities/credentials.go
+++ identities/credentials.go
@@ -1,6 +1,9 @@
 package identities
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Credentials struct {
 	Username string
@@ -13,11 +16,29 @@ func (p *Person) Credentials() *Credentials {
 	if p.up != nil {
 		return p.up
 	}
+	name := p.firstName
+	if len(name) < 3 {
+		name = padding(p.firstName)
+	}
+
+	lastName := normalize(p.lastName)
+	if len(lastName) < 3 {
+		lastName = padding(p.lastName)
+	}
 	//This supposes no Names/Surnames are shorter than 3
 	up := &Credentials{
-		Username: fmt.Sprintf("%s%s%d", p.firstName[:3], p.lastName[:3], p.birthDate.Year()),
-		Password: randString([]rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), 6) + randString([]rune(".-!"), 2),
+		Username: fmt.Sprintf("%s%s%d", name[:3], lastName[:3], p.birthDate.Year()),
+		Password: randString([]rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), 6) +
+			randString([]rune(".-!"), 2),
 	}
 	p.up = up
 	return up
+}
+
+func normalize(name string) string {
+	return strings.Join(strings.Split(name, "'"), "X")
+}
+
+func padding(name string) string {
+	return fmt.Sprintf("%x3s", name)
 }
